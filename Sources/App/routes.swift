8698ce4d3d14207442288d03079878e8995bd44a
Christopher Batin <christopher.batin@ee.co.uk>
2018-10-25 12:47:01 +0100

    Init commit

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,20 +1,52 @@
 import Vapor
-
+import PushNotifications
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
-    // Basic "It works" example
-    router.get { req in
-        return "It works!"
-    }
-    
-    // Basic "Hello, world!" example
-    router.get("hello") { req in
-        return "Hello, world!"
+
+    router.get("push") { req -> String in
+        do {
+            try PushNotificationService.send(message: "Hello, new pizza offers are available!")
+            return "Push Success"
+        } catch {
+            return "Push Failed"
+        }
     }
 
-    // Example of configuring a controller
-    let todoController = TodoController()
-    router.get("todos", use: todoController.index)
-    router.post("todos", use: todoController.create)
-    router.delete("todos", Todo.parameter, use: todoController.delete)
+
+    router.post(PizzaOffer.self, at: "push/offer/") { req, data -> String in
+        do {
+            try PushNotificationService.send(message: "Hello, \(data.pizzaName) has this offer: \(data.pizzaOffer)")
+            return "Push Success"
+        } catch {
+            return "Push Failed"
+        }
+    }
+}
+
+
+
+struct PizzaOffer: Content {
+    let pizzaName: String
+    let pizzaOffer: String
+}
+
+class PushNotificationService {
+
+    class func send(message: String) throws {
+        let pushNotifications = PushNotifications(instanceId: "6b87bf51-26e3-4117-a312-281c64817f0b" , secretKey:"0B6D73FAEBFC996A863B91B7F5AB9555E1A96042F9AA645E4334F44E4DE08FD6")
+        let interests = ["pizza"]
+        let publishRequest = [
+            "apns": [
+                "aps": [
+                    "alert": [
+                        "title": "Pizza Offer",
+                        "body": message
+                    ]
+                ]
+            ]
+        ]
+        try pushNotifications.publish(interests, publishRequest, completion: { publishID in
+            print("Published \(publishID)")
+        })
+    }
 }
